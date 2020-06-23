69bcb8d1211f513b8372a9f6fc8be4accb3b9e11
rmachado <rodrigo.machado3.14@hotmail.com>
2020-06-23 01:25:58 -0300

    added filter_data

--- run.py
+++ run.py
@@ -55,5 +55,21 @@ def demo_jinja2_of_dict():
                            name='rmachado')
 
 
+@app.route('/filters')
+def filter_data():
+    movies_dict = {
+        'autopsy of jane done': 02.14,
+        'neon demon': 3.20,
+        'ghost  in a shell': 1.50,
+        'kong: skull island': 3.50,
+        'john wick 2': 02.52,
+        'spiderman - homecoming': 1.48
+    }
+    return render_template('filter_data.html',
+                           movies=movies_dict,
+                           name=None,
+                           film='a christmas machado')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
