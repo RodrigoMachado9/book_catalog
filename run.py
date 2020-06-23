c4081729515cd0933732d1258935dd6c5fe53cf8
rmachado <rodrigo.machado3.14@hotmail.com>
2020-06-23 17:20:34 -0300

    config postgresql

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config.update(
+    SECRET_KEY='topsecret',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgresql:topsecret@localhost/catalog_db',
+    SQLALCHEMY_TRACK_MODIFICATION=False)
+
+sb = SQLAlchemy(app)
 
 
 @app.route('/index')
