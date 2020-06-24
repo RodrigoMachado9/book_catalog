164d65a753a127e44c9dbcc67c56c3ced9517550
rmachado <rodrigo.machado3.14@hotmail.com>
2020-06-24 00:13:59 -0300

    adjustment

--- run.py
+++ run.py
@@ -99,12 +99,11 @@ class Publication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return "The id is {}, name is {}".format(self.id, self.name)
+        return "The name is {}".format(self.name)
 
 
 class Book(db.Model):
