49eaf56fd7fc6613487335f6f51fe93b38d78394
rmachado <rodrigo.machado3.14@hotmail.com>
2020-06-23 23:37:02 -0300

    added model

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -104,6 +105,34 @@ class Publication(db.Model):
 
     def __repr__(self):
         return "The id is {}, name is {}".format(self.id, self.name)
+
+
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False, index=True)
+    author = db.Column(db.String(350))
+    avg_rating = db.Column(db.Float)
+    book_format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    # relationship > book 1 ------ 1 publication
+    pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+
+    def __init__(self, title, author, avg_rating, book_format, image, num_pages, pub_id):
+        self.title = title
+        self.author = author
+        self.avg_rating = avg_rating
+        self.book_format = book_format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+
+    def __repr__(self):
+        return "{} by {}".format(self.title, self.author)
 
 
 if __name__ == '__main__':
