9e9860682f7e0a1cb621d24707e3f79af0e34f09
rmachado <rodrigo.machado3.14@hotmail.com>
2020-06-25 17:43:28 -0300

    function for validate email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,12 +1,19 @@
 # app/auth/forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from app.auth.models import User
+
+
+def email_exists(form, field):
+    email = User.query.filter_by(user_email=field.data).first()
+    if email:
+        raise ValidationError('Email Already Exists')
 
 
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(3, 15, message='between 3 to 15 characters')])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(), email_exists])
     # email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password',
                              validators=[DataRequired(), Length(5), EqualTo('confirm', message='password must match')])
