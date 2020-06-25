0174c6271dbded719340e442b9b764b01b6548a0
rmachado <rodrigo.machado3.14@hotmail.com>
2020-06-25 14:50:30 -0300

    in progress

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Whats your Name')
-    email = StringField('Enter your Email')
+    name = StringField('Name', validators=[DataRequired(), Length(3, 15, message='between 3 to 15 characters')])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(5), EqualTo('confirm',
+                                                                                        message='password must match')])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
     submit = SubmitField('Register')
 
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,18 +1,20 @@
-from flask import render_template, request
+# app/auth/routes.py
+from flask import render_template, flash, redirect, url_for
 from app.auth.forms import RegistrationForm
 from app.auth import authentication as at
+from app.auth.models import User
 
 
 @at.route('/register', methods=['GET', 'POST'])
 def register_user():
-    name = None
-    email = None
+
     form = RegistrationForm()
 
-
-
-    if request.method == 'POST':
-        name = form.name.data
-        email = form.email.data
-
-    return render_template('registration.html', form=form, name=name, email=email)
+    if form.validate_on_submit():
+        User.create_user(
+            user=form.name.data,
+            email=form.email.data,
+            password=form.password.data)
+        flash('Registration Successful')
+        return redirect(url_for('at.login_user'))    
+    return render_template('registration.html', form=form)
